dd36236850d431dd1b4f2a0d1369bf687873176d
Egor Belousov <egor0288354@yandex.ru>
2021-04-07 00:06:37 +0300

    Task #2 (Cabin color)

--- server.py
+++ server.py
@@ -12,5 +12,11 @@ def distribution():
     return render_template('distribution.html', persons=persons)
 
 
+@app.route('/table/<sex>/<int:age>')
+def table(sex, age):
+    age = 'old' if age >= 21 else 'young'
+    return render_template('table.html', sex=sex, age=age)
+
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
